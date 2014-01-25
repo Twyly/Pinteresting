6cf0f6d07f9445a06c2b3b4abffc5037ead5f1b3
Teddy Wyly <wylynout@gmail.com>
2014-01-24 23:56:29 -0500

    implemented devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Pinterest::Application.routes.draw do
-  devise_for :installs
+  devise_for :users
+  #devise_for :installs
   root "pages#home"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
